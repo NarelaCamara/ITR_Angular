5087a8d5e845cd9f08f055d12ba8f48cd4cf0535
RoxanaCamara <roxana.camara07@gmail.com>
2019-11-15 16:28:05 -0300

    FM

--- src/app/mascotas/mascotas-listar/mascotas-listar.component.ts
+++ src/app/mascotas/mascotas-listar/mascotas-listar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MascotasService } from '../shared/mascotas.service';
 import { Mascota } from '../shared/mascota';
+import { Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -8,17 +9,37 @@ import { Mascota } from '../shared/mascota';
   templateUrl: './mascotas-listar.component.html',
   styleUrls: ['./mascotas-listar.component.css']
 })
-export class MascotasListarComponent{
+export class MascotasListarComponent {
 
   public mascotas: Array<Mascota> = [];
 
-  constructor(private mascotasService: MascotasService) { }
+  public mascotaForm = this.fb.group({
+    id: [''],
+    nombre: ['', Validators.required],
+    tipo: ['', Validators.required],
+    edad: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    descripcion: ['', Validators.required]
+  });
+
+
+
+  constructor(private fb: FormBuilder, private mascotasService: MascotasService) { }
+
 
   ngOnInit() {
     this.mascotasService.getMascotas().subscribe((data) => {
       this.mascotas = data;
     })
   }
+
+
+  borrarMascota(id: number) {
+    this.mascotasService.deleteMascota(id).subscribe(data => {
+      this.mascotasService.getMascotas( ).subscribe( listaActual => this.mascotas = listaActual );
+    })
+  }
+
+
 
 }
 
--- src/app/mascotas/shared/mascotas.service.ts
+++ src/app/mascotas/shared/mascotas.service.ts
@@ -11,28 +11,38 @@ export class MascotasService {
 
   baseUrl: String = "http://localhost:8090/mascotas";
 
-  constructor(private httpClient: HttpClient,private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   public getMascotas() {
     return this.httpClient.get<Array<Mascota>>(`${this.baseUrl}`);
   }
 
-  
+
   public addMascota(mascota: Mascota) {
     return this.httpClient.post<Mascota>(`${this.baseUrl}`, mascota).subscribe(data => {
       this.router.navigate(['/mascotas-listar']);
-});
+    });
 
   }
 
-  public getMascota(id:number){
+  public getMascota(id: number) {
     return this.httpClient.get<Mascota>(`${this.baseUrl}/${id}`);
   }
+
 
   public editMascota(mascota: Mascota) {
     return this.httpClient.put<Mascota>(`${this.baseUrl}`, mascota).subscribe(data => {
       this.router.navigate(['/mascotas-listar']);
-});
+    });
   }
+
+
+  public deleteMascota(id: number) {
+    return this.httpClient.delete<Mascota>(`${this.baseUrl}/${id}` );
+  }
+
+
+
+
 
 }
